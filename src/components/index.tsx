7fd60e4c0d768dd5fac7663fed3d305a984b922c
Lucas Roseti <lc.roseti@gmail.com>
2023-05-21 11:00:14 -0300

    Improvements and new memory page
    - Default layout for all app
    - Middleware to access the memories path
    - New memory page layout
    - Logout api route
    - Tailwind forms

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
 import { SignIn } from '@/components/SignIn'
 
-export { Copyright, EmptyMemories, Hero, SignIn }
+export { Copyright, EmptyMemories, Hero, SignIn, Profile }
